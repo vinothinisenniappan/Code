609188c5c10c95bc0f0d2a66459be5c913a3b475
vino-01 <vinothinisenniappan1204@gmail.com>
2026-06-11 22:23:53 +0530

    Day-1

---
+++ Maximum.java
@@ -0,0 +1,24 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int arr[] = new int[a];
+        for(int i=0;i<a;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int max=0;
+        for(int i=0;i<a;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+        
+            System.out.print(max);
+    }
+}
---
+++ Odd_Even.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+            if(a%2==0)
+            {
+                System.out.print("even");
+            }
+            else
+            {
+                System.out.print("odd");
+            }
+    }
+}
