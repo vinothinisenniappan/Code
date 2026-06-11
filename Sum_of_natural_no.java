72fbca60eac4af8f5b02f804d76abcff2e70c72c
vino-01 <vinothinisenniappan1204@gmail.com>
2026-06-11 22:28:35 +0530

    Day-1

---
+++ Sum_of_natural_no.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int num =0;
+        
+        for(int i=1;i<=a;i++)
+        {
+            num+=i;
+        }
+        System.out.print(num);
+        
+    }}
