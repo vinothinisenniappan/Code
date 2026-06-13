9415197b4a91012f95f6679fe3402747efbdad58
vino-01 <vinothinisenniappan1204@gmail.com>
2026-06-13 23:14:10 +0530

    Day-3

---
+++ Five_table.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for(int i=a;i<=a;i++)
+        {
+            for(int j=1;j<=10;j++)
+            {
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+        }
+    }
+}
---
+++ Number_count.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int count = 0;
+        while(a>0)
+        {
+            a = a/10;
+            count++;
+        }
+        System.out.print(count);
+    }
+}
