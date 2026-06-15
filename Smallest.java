1742169ed47c76eefd73bcf8ce79e207adbb1c3e
vino-01 <vinothinisenniappan1204@gmail.com>
2026-06-15 15:51:06 +0530

    Day-5

---
+++ Smallest.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int s = 9;
+        while(a>0)
+        {
+            int d = a%10;
+            if(d<s)
+            {
+                s = d;
+            }
+            a=a/10;
+        }
+        System.out.print(s);
+    }
+}
