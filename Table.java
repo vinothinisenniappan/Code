1996b8f0782ea24ea99c087a2aee6da0283a7441
vino-01 <vinothinisenniappan1204@gmail.com>
2026-06-18 13:20:18 +0530

    Commit

---
+++ Table.java
@@ -0,0 +1,12 @@
+import java.util.*; 
+class Main { 
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in); 
+         int a = sc.nextInt(); int j=1; 
+         while(j<=10) 
+            { 
+                System.out.println(a+"*"+j+"="+a*j); 
+                j++;
+             } 
+            } 
+        }
