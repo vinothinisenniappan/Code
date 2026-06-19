0bf04818ead287d52f4277b3d7293db6d32ee1b0
vino-01 <vinothinisenniappan1204@gmail.com>
2026-06-19 23:42:42 +0530

    day-6

---
+++ Largest_array.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Largest_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int arr[] = int new[a];
+        for (int idx = 0; idx < arr.length; idx++) {
+            Object elem = arr[idx];
+            
+        }
+    }
+}
