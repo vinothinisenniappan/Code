c623320bdf9e4798c098cdd0ff02cfa35da05798
vino-01 <vinothinisenniappan1204@gmail.com>
2026-07-14 20:59:50 +0530

    commit

--- Armstrong.java
+++ Armstrong.java
@@ -1,24 +1,22 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 import java.util.*;
-class Main
-{
-    public static void main(String args[])
-    {
+class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int val = a;
-        int arm = 0;
-        int k = 0;
-        while(a>0)
-        {
-            int d = a%10;
-            arm = d*d*d;
-            k = arm+k;
+        int temp = a;
+        int num=0;
+        while(a>0){
+           int digit = a%10;
+           int k = digit*digit*digit;
+            num +=k; 
             a = a/10;
         }
-        if(k == val)
-        System.out.print("Armstrong");
-        else
-        System.out.print("Not a Armstrong");
-
+        if(num==temp){
+        System.out.println("Armstrong");}
+        else{
+        System.out.print("Not armstrong");
+    }
     }
 }
