a1577f7604fc5d149f5027b381567b2680a9a102
vino-01 <vinothinisenniappan1204@gmail.com>
2026-07-30 22:54:01 +0530

    lower_case

---
+++ toLowercase.java
@@ -0,0 +1,10 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.print(s.toLowerCase());
+        }
+}
