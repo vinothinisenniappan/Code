1de69854d045dc4c1acd35c3497225900c429491
vino-01 <vinothinisenniappan1204@gmail.com>
2026-07-30 22:52:15 +0530

    revers_str_stringbuilder

---
+++ reverse_str_stringbuilder.java
@@ -0,0 +1,11 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        StringBuilder st =  new StringBuilder(s);
+        System.out.print(st.reverse());
+        }
+}
