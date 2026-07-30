6e6bfda2bc87d6a1801f9aa21ac109100581a860
vino-01 <vinothinisenniappan1204@gmail.com>
2026-07-30 22:40:01 +0530

    each char

---
+++ Print_each_character.java
@@ -0,0 +1,13 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            System.out.println(ch);
+        }
+        }
+}
