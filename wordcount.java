66dac580d2363496b88e0edec5f0ddafea3d3425
vino-01 <vinothinisenniappan1204@gmail.com>
2026-07-31 21:48:37 +0530

    wordcount

---
+++ wordcount.java
@@ -0,0 +1,12 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String[] a = s.split("\\s+");
+        System.out.print(a.length);
+        
+    }
+}
