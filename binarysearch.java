2a089c8bf92740a199e9b8d6334bf4326fee36d5
vino-01 <vinothinisenniappan1204@gmail.com>
2026-08-06 09:14:06 +0530

    commit

---
+++ binarysearch.java
@@ -0,0 +1,17 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        for(int i=0;i<a;i++){
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+            Arrays.sort(arr);
+            int value = Arrays.binarySearch(arr,target);
+            System.out.println(value);
+    }
+}
