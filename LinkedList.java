a311b42b6b35a9e7c1899bb8e355f50a3989bfe2
vino-01 <vinothinisenniappan1204@gmail.com>
2026-08-09 22:17:13 +0530

    co

---
+++ LinkedList.java
@@ -0,0 +1,37 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+               Node head = null;
+        Node tail = null;
+        for(int i=0;i<a;i++){
+        int value = sc.nextInt();
+        Node n = new Node(value);
+ 
+        if(head==null){
+            head = n;
+            tail = n;
+        }
+        else{
+            tail.next = n;
+            tail = n;
+        }
+        }
+        Node temp = head;
+        while(temp!=null){
+        System.out.println(temp.data+" ");
+        temp = temp.next;
+        }
+    }
+}
